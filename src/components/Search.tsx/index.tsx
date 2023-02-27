27e95725b8f44c080fe74fc1cdb2692f6c7a701f
Joyce dos Santos Silva <joycebigoli@gmail.com>
2023-02-27 19:36:34 -0300

    criando funcionalidade no input para fazer a busca com a tecla enter

--- src/components/Search.tsx/index.tsx
+++ src/components/Search.tsx/index.tsx
@@ -2,17 +2,23 @@ type SearchProps = {
     loadUser: (userName: string) => Promise<void>
 }
 import {BsSearch} from 'react-icons/bs'
-import { useState } from "react"
+import { useState, KeyboardEvent } from "react"
 import style from './style.module.css'
 
 const Search = ({loadUser}:SearchProps) => {
     const [userName , setUserName] = useState('')
+    const handleKeyDown = (e:KeyboardEvent) => {
+        if (e.key === "Enter"){
+           loadUser(userName);
+        }
+    };
   return (
     <div className={style.search}> 
         <h2>Busque por um usuário</h2>
         <p>Conheça seus melhores Repositórios</p>
         <div className={style.search_container}>
             <input 
+            onKeyDown={handleKeyDown}
             type='text'
             placeholder='Digite o nome do usuário'
             onChange={(e) => setUserName(e.target.value) }/>
