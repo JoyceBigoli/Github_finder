e423262c23e35fa96d2da06ed4e3aad8fe444d6c
Joyce dos Santos Silva <joycebigoli@gmail.com>
2023-02-28 08:26:35 -0300

    criando estilização do compnent user

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -1,6 +1,7 @@
 import { UserProps } from "../../types/user"
 import {MdLocationPin}  from "react-icons/md"
 import { Link } from "react-router-dom"
+import style from './style.module.css'
 
 export const User= ({
     login,
@@ -10,25 +11,23 @@ export const User= ({
     followers
 }:UserProps) => {
   return (
-    <div>
+    <div className={style.user}>
         <img src={avatar_url} alt={login}></img>
         <h2>{login}</h2>
         {location && 
-        <p>
+        <p className={style.location}>
             <MdLocationPin/>
             <span>{location}</span> 
         </p>
         }
-        <div>
+        <div className={style.stats}>
             <div>
-                <p>seguidores:</p>
-                <p>{followers}</p>
-            </div>
+                <p>Seguidores:</p>
+                <p className={style.number}>{followers}</p>
+            </div>            
             <div>
-                <div>
-                    <p>Seguindo:</p>
-                    <p>{following}</p>
-                </div>
+                <p>Seguindo:</p>
+                <p className={style.number}>{following}</p>
             </div>
         </div>
         <Link to={`/repos/${login}`}>Ver melhores projetos</Link>
